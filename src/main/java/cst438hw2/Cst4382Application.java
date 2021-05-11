4ba00cd629279babe21ec9dbc58ae77fe539c6b1
Jordan Bienz <jordanbienz@gmail.com>
2021-05-10 21:21:19 -0700

    initial commit

---
+++ src/main/java/cst438hw2/Cst4382Application.java
@@ -0,0 +1,13 @@
+package cst438hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cst4382Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cst4382Application.class, args);
+	}
+
+}
